2385a18726f8d9574aa7f3ce31af7c92a9b55eda
George Stavrinos <stavrinosgeo@gmail.com>
2018-09-23 13:56:42 +0300

    First step of the OpenStreetMaps-Kotlin integration. For now, you can just open the map

---
+++ README.md
@@ -0,0 +1,4 @@
+An Android application thta wakes you up near the GPS location you provided. Sleep on the buss freely!
+
+A project to familiarize myself with Kotlin and OpenStreetMaps! :)
+
---
+++ app/src/main/java/gstavrinos/destinationalarm/MainActivity.kt
@@ -0,0 +1,64 @@
+package gstavrinos.destinationalarm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
+import android.preference.PreferenceManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.api.IMapController
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    var map: MapView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //inflate and create the map
+        setContentView(R.layout.activity_main)
+
+        map = findViewById(R.id.mapview) as MapView
+        map!!.setTileSource(TileSourceFactory.MAPNIK)
+        map!!.setBuiltInZoomControls(true)
+        map!!.setMultiTouchControls(true)
+        val mapController = map!!.controller
+        mapController.setZoom(5.0)
+        val startPoint = GeoPoint(37.981912,23.727447)
+        mapController.setCenter(startPoint)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map!!.onResume() //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map!!.onPause()  //needed for compass, my location overlays, v6.0.0 and up
+    }
+}
